77da8385e8340f91d3ef2adba31553cf33fb395e
yethranayeh <aktasalper99@gmail.com>
2021-12-23 14:12:38 +0300

    basic visuals and board generation

--- script.js
+++ script.js
@@ -3,17 +3,46 @@
 // Game Board Module
 const gameBoard = {
 	init: function () {
-		this.createBoard();
+		this.board = this.createBoard();
 	},
 	createBoard: function () {
-		console.log("Create Board");
-		return;
+		return ["x", "o", false, "o", "x", "o", false, "o", "x"];
 	}
 };
 gameBoard.init();
 
 // Display Controller Module
-const displayController = {};
+const displayController = {
+	init: function () {
+		this.playerInfo = document.querySelector(".player-info");
+		this.board = document.querySelector(".game-board");
+		this.displayBoard();
+	},
+	displayBoard: function () {
+		gameBoard.board.forEach((content) => {
+			let gameBox = document.createElement("div");
+			gameBox.classList.add("game-box");
+			let gameBoxContent = document.createElement("p");
+			gameBoxContent.textContent = content ? content : "";
+			gameBox.appendChild(gameBoxContent);
+			this.board.appendChild(gameBox);
+		});
+		this.boxes = this.board.querySelectorAll(".game-box");
+		console.log(this.boxes);
+	},
+	displayPlayer: function (player) {
+		let playerDOM = this.playerInfo.querySelector("#player");
+		playerDOM.textContent = player.playerName;
+	},
+	displayOpponent: function (opponent) {
+		let opponentDOM = this.playerInfo.querySelector("#opponent");
+		opponentDOM.textContent = opponent ? opponent : "Computer22";
+	},
+	setPlayerInput: function () {
+		return;
+	}
+};
+displayController.init();
 
 // Player Factory
 const playerFactory = (playerName) => {
@@ -21,4 +50,5 @@ const playerFactory = (playerName) => {
 };
 
 const player = playerFactory("James");
-console.log(player.playerName);
+displayController.displayPlayer(player);
+displayController.displayOpponent();
